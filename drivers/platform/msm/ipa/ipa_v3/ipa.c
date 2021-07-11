e0467d2bcfed333a700ea9ee852f89a428b2bfb1
Vaisakh Murali <mvaisakh@statixos.com>
2021-07-11 20:36:01 +0530

    drivers: ipa_v3: Conditionally compile out ipa wakelock code
    * This is spurious and does not get destroyed, which keeps this
      wakelock as pending. Remove this code to save power.
    
    * To enable these wakelocks again, pass `-DIPA_WAKELOCKS` as a
      cflag.
    
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>
    
    [dakkshesh07: adapted for k4.9's ipa_3 drivers]
    Signed-off-by: Dakkshesh <dakkshesh5@gmail.com>

--- drivers/platform/msm/ipa/ipa_v3/ipa.c
+++ drivers/platform/msm/ipa/ipa_v3/ipa.c
@@ -4183,7 +4183,6 @@ void ipa3_dec_client_disable_clks_no_block(
  */
 void ipa3_inc_acquire_wakelock(void)
 {
-#if 0
 	unsigned long flags;
 
 	spin_lock_irqsave(&ipa3_ctx->wakelock_ref_cnt.spinlock, flags);
@@ -4193,7 +4192,7 @@ void ipa3_inc_acquire_wakelock(void)
 	IPADBG_LOW("active wakelock ref cnt = %d\n",
 		ipa3_ctx->wakelock_ref_cnt.cnt);
 	spin_unlock_irqrestore(&ipa3_ctx->wakelock_ref_cnt.spinlock, flags);
-#endif
+
 }
 
 /**
@@ -4206,7 +4205,6 @@ void ipa3_inc_acquire_wakelock(void)
  */
 void ipa3_dec_release_wakelock(void)
 {
-#if 0
 	unsigned long flags;
 
 	spin_lock_irqsave(&ipa3_ctx->wakelock_ref_cnt.spinlock, flags);
@@ -4216,7 +4214,6 @@ void ipa3_dec_release_wakelock(void)
 	if (ipa3_ctx->wakelock_ref_cnt.cnt == 0)
 		__pm_relax(&ipa3_ctx->w_lock);
 	spin_unlock_irqrestore(&ipa3_ctx->wakelock_ref_cnt.spinlock, flags);
-#endif
 }
 #endif
 
@@ -5722,12 +5719,11 @@ static int ipa3_pre_init(const struct ipa3_plat_drv_res *resource_p,
 		goto fail_device_create;
 	}
 
-#if 0
+#ifdef IPA_WAKELOCKS
 	/* Create a wakeup source. */
 	wakeup_source_init(&ipa3_ctx->w_lock, "IPA_WS");
 	spin_lock_init(&ipa3_ctx->wakelock_ref_cnt.spinlock);
 #endif
-
 	/* Initialize Power Management framework */
 	if (ipa3_ctx->use_ipa_pm) {
 		result = ipa_pm_init(&ipa3_res.pm_init);
