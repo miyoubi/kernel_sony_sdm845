c980b2151b8924a0b3f62001f5dda6fe6e9e412f
Pzqqt <821026875@qq.com>
2025-04-26 13:42:07 +0800

    security: lsm_audit: Add a userspace node to control enable/disable avc log
    Sometimes, a large number of avc log spam will interferes with debugging.
    But avc log is important to fix the sepolicy rules of rom.
    
    So we add a userspace node to enable or disable it at any time.
    
    Enable avc log (default):
    echo 0 > /sys/module/lsm_audit/parameters/disable_audit_log
    
    Disable avc log:
    echo 1 > /sys/module/lsm_audit/parameters/disable_audit_log
    
    Signed-off-by: Pzqqt <821026875@qq.com>

--- security/lsm_audit.c
+++ security/lsm_audit.c
@@ -30,6 +30,10 @@
 #include <linux/dccp.h>
 #include <linux/sctp.h>
 #include <linux/lsm_audit.h>
+#include <linux/module.h>
+ 
+uint disable_audit_log = 0;
+module_param(disable_audit_log, uint, 0644);
 
 /**
  * ipv4_skb_to_auditdata : fill auditdata from skb
@@ -433,6 +437,9 @@ void common_lsm_audit(struct common_audit_data *a,
 {
 	struct audit_buffer *ab;
 
+	if (disable_audit_log)
+ 		return;
+ 
 	if (a == NULL)
 		return;
 	/* we use GFP_ATOMIC so we won't sleep */
