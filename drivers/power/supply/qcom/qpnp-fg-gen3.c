dddf7282665c15eedc0d0c4324c4613ae0453e88
ekkusa <dayesofficial@gmail.com>
2025-04-27 15:18:31 +0800

    qcom: qpnp-fg-gen3: queue works into a power efficent WQ ~ FIXUP!

--- drivers/power/supply/qcom/qpnp-fg-gen3.c
+++ drivers/power/supply/qcom/qpnp-fg-gen3.c
@@ -6490,112 +6490,6 @@ static int fg_parse_dt(struct fg_chip *chip)
 	return 0;
 }
 
-<<<<<<< HEAD
-=======
-	int last_batt_soc = chip->param.batt_soc;
-	int time_since_last_change_sec;
-
-	struct timespec last_change_time = chip->param.last_soc_change_time;
-
-	calculate_delta_time(&last_change_time, &time_since_last_change_sec);
-
-	if (chip->param.batt_temp > 150) {
-		/* Battery in normal temperture */
-		if (chip->param.batt_ma < 0)
-			delta_time = time_since_last_change_sec / 30;
-		else {
-			calculate_average_current(chip);
-			if (chip->param.batt_ma_avg > 1000000)
-				delta_time = time_since_last_change_sec / 20;
-			else
-				delta_time = time_since_last_change_sec / 60;
-		}
-	} else {
-		/* Battery in low temperture */
-		calculate_average_current(chip);
-		/* Calculated average current > 1000mA */
-		if (chip->param.batt_ma_avg > 1000000)
-			/* Heavy loading current, ignore battery soc limit*/
-			delta_time = time_since_last_change_sec / 10;
-		else
-			delta_time = time_since_last_change_sec / 20;
-	}
-
-
-	if (delta_time < 0)
-		delta_time = 0;
-
-	soc_changed = min(1, delta_time);
-
-	if (last_batt_soc >= 0) {
-		if (last_batt_soc < chip->param.batt_raw_soc &&
-					chip->param.batt_ma < 0)
-		/* Battery in charging status
-		 * update the soc when resuming device
-		 */
-		last_batt_soc = chip->param.update_now ?
-			chip->param.batt_raw_soc : last_batt_soc + soc_changed;
-		else if (last_batt_soc > chip->param.batt_raw_soc
-					&& chip->param.batt_ma > 0)
-		  /* Battery in discharging status
-		   * update the soc when resuming device
-		   */
-		last_batt_soc = chip->param.update_now ?
-			chip->param.batt_raw_soc : last_batt_soc - soc_changed;
-		else if (last_batt_soc != 100
-					&& chip->param.batt_raw_soc >= 95
-					&& chip->charge_status == POWER_SUPPLY_STATUS_FULL)
-
-			last_batt_soc = chip->param.update_now ?
-				100 : last_batt_soc + soc_changed;
-
-		chip->param.update_now = false;
-	} else {
-		last_batt_soc = chip->param.batt_raw_soc;
-	}
-
-	if (chip->param.batt_soc != last_batt_soc) {
-		chip->param.batt_soc = last_batt_soc;
-		chip->param.last_soc_change_time = last_change_time;
-		power_supply_changed(chip->batt_psy);
-	}
-
-	pr_info("soc:%d, last_soc:%d, raw_soc:%d, soc_changed:%d.\n",
-				chip->param.batt_soc, last_batt_soc,
-				chip->param.batt_raw_soc, soc_changed);
-}
-
-#define MONITOR_SOC_WAIT_MS					1000
-#define MONITOR_SOC_WAIT_PER_MS             10000
-static void soc_monitor_work(struct work_struct *work)
-{
-	int rc;
-	struct delayed_work *dwork = to_delayed_work(work);
-	struct fg_chip *chip = container_of(dwork,
-				struct fg_chip, soc_monitor_work);
-
-	rc = fg_get_battery_current(chip, &chip->param.batt_ma);
-	if (rc < 0)
-		pr_err("failded to get battery current, rc=%d\n", rc);
-
-	rc = fg_get_prop_capacity(chip, &chip->param.batt_raw_soc);
-	if (rc < 0)
-		pr_err("failed to get battery capacity, rc=%d\n", rc);
-
-	rc = fg_get_battery_temp(chip, &chip->param.batt_temp);
-	if (rc < 0)
-		pr_err("failed to get battery temperature, rc=%d\n", rc);
-
-	fg_battery_soc_smooth_tracking(chip);
-
-	pr_info("soc:%d, raw_soc:%d, c:%d, s:%d\n",
-				chip->param.batt_soc, chip->param.batt_raw_soc,
-				chip->param.batt_ma, chip->charge_status);
-	schedule_delayed_work(&chip->soc_monitor_work,
-				msecs_to_jiffies(MONITOR_SOC_WAIT_PER_MS));
-}
-
->>>>>>> 11b41b4e6f30 (qcom: qpnp-fg-gen3: queue works into a power efficent WQ)
 static void fg_cleanup(struct fg_chip *chip)
 {
 	int i;
@@ -7219,14 +7113,7 @@ static int fg_gen3_resume(struct device *dev)
 	if (rc < 0)
 		pr_err("Error in configuring ESR timer, rc=%d\n", rc);
 
-<<<<<<< HEAD
 	schedule_delayed_work(&chip->ttf_work, 0);
-=======
-	queue_delayed_work(system_power_efficient_wq,
-		&chip->esr_timer_config_work, 0);
-	queue_delayed_work(system_power_efficient_wq,
-		&chip->ttf_work, 0);
->>>>>>> 11b41b4e6f30 (qcom: qpnp-fg-gen3: queue works into a power efficent WQ)
 	if (fg_sram_dump)
 		queue_delayed_work(system_power_efficient_wq,
 			&chip->sram_dump_work,
